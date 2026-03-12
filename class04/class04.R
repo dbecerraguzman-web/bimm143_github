67c573703c8ba281ae889c43000a40fba3289227
Dariana Becerra Guzman <dbecerraguzman@ucsd.edu>
2026-03-12 11:20:39 -0700

    moving files

---
+++ class04/class04.R
@@ -0,0 +1,12 @@
+#' ---
+#' title: "Lab Class 4"
+#' author: "Dariana Becerra Guzman"
+#' ---
+
+x <- 1:50
+plot(x, sin(x))
+plot (x, sin(x), typ="l", col="red", lwd=15)
+log(10, base = 10)
+
+
+install.packages("tinytex")
